aa7bed05743119c7239771b28b421fec747d77e3
huhaonan <huhaonan@sics.ac.cn>
2024-07-08 18:10:56 +0800

    feat: 添加页面

---
+++ views/功能页面/function-pages/awesome/三种模式导航菜单/js/script.js
@@ -0,0 +1,13 @@
+var menus = document.querySelectorAll('.menu')
+menus.forEach(function (menu) {
+    var menuItems = menu.querySelectorAll('.menu-item')
+    menuItems.forEach(function (item) {
+        item.addEventListener('click', function (e) {
+            menuItems.forEach(function (item) {
+                return item.classList.remove('active')
+            })
+            var target = e.target;
+            target.classList.add('active')
+        })
+    })
+})
